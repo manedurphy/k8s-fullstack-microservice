2ce2ad83bfa9f9cda416b61487951687b7c57cb7
Dane Murphy <danem7@gmail.com>
2021-05-21 00:16:28 -0700

    added k8s definitions for deployment on linode

--- artillery/generateIds.js
+++ artillery/generateIds.js
@@ -1,16 +1,16 @@
 const createWriter = require('csv-writer').createObjectCsvWriter;
 const csvWriter = createWriter({
-    path: './artillery/ids.csv',
+    path: './ids.csv',
     header: [{ id: 'id', title: 'space' }],
 });
 
 const data = [];
 
-// for (let i = 1; i <= 1000; i++) {
-//     data.push({ id: i });
-// }
-for (let i = 9000000; i <= 10000000; i++) {
+for (let i = 1; i <= 1000; i++) {
     data.push({ id: i });
 }
+// for (let i = 9000000; i <= 10000000; i++) {
+//     data.push({ id: i });
+// }
 
 csvWriter.writeRecords(data).then(() => console.log('done!'));
